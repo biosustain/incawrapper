a002bdcba1efb178b131534abd76b6b1082677be
viktorht <viktorht@yahoo.dk>
2023-01-16 16:23:12 +0100

    WIP: comment about matlab obj structure

---
+++ dev_reimport.py
@@ -0,0 +1,102 @@
+#%%
+import pandas as pd
+import numpy as np
+from BFAIR.mfa.INCA import INCA_reimport
+import os
+import dotenv
+import logging 
+
+# Set working directory
+os.chdir(dotenv.find_dotenv().replace('.env', ''))
+
+# setup logging 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+# Setup data paths 
+filename = os.path.join("tests", "test_data", "MFA_modelInputsData", "simple_model", "simple_model.mat")
+simulation_info = pd.read_csv(os.path.join("tests", "test_data", "MFA_modelInputsData", "simple_model", "experimentalMS.csv"), index_col=0)
+simulation_id = 'exp1'
+
+reimport_data = INCA_reimport()
+
+#%%
+fittedData2, fittedFluxes2, fittedFragments2, fittedMeasuredFluxes2, fittedMeasuredFragments2, fittedMeasuredFluxResiduals2, fittedMeasuredFragmentResiduals2, simulationParameters2 = reimport_data.reimport(filename, simulation_info, simulation_id)
+# %%
+
+from scipy.io import loadmat, matlab
+def load_mat(filename):
+    """
+    This function should be called instead of direct scipy.io.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    """
+
+    def _check_vars(d):
+        """
+        Checks if entries in dictionary are mat-objects. If yes
+        todict is called to change them to nested dictionaries
+        """
+        for key in d:
+            if isinstance(d[key], matlab.mio5_params.mat_struct):
+                d[key] = _todict(d[key])
+            elif isinstance(d[key], np.ndarray):
+                d[key] = _toarray(d[key])
+        return d
+
+    def _todict(matobj):
+        """
+        A recursive function which constructs from matobjects nested dictionaries
+        """
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, matlab.mio5_params.mat_struct):
+                d[strg] = _todict(elem)
+            elif isinstance(elem, np.ndarray):
+                d[strg] = _toarray(elem)
+            else:
+                d[strg] = elem
+        return d
+
+    def _toarray(ndarray):
+        """
+        A recursive function which constructs ndarray from cellarrays
+        (which are loaded as numpy ndarrays), recursing into the elements
+        if they contain matobjects.
+        """
+        if ndarray.dtype != 'float64':
+            elem_list = []
+            for sub_elem in ndarray:
+                if isinstance(sub_elem, matlab.mio5_params.mat_struct):
+                    elem_list.append(_todict(sub_elem))
+                elif isinstance(sub_elem, np.ndarray):
+                    elem_list.append(_toarray(sub_elem))
+                else:
+                    elem_list.append(sub_elem)
+            return np.array(elem_list)
+        else:
+            return ndarray
+
+    data = loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_vars(data)
+## Expected use of the reimport object
+# reimport_data = INCA_reimport(matlab_file)
+
+# %%
+# matlab_obj['f']['par']
+# This is an array which holds a dictionary for each parameters (reaction) in the model
+# Some of the keys contain lists and I believe chi2s, and vals these are one for each restart,
+# while cor and cov contains one for each reaction.
+
+# matlab_obj['f']['mnt']
+# This is an array which holds a dictionary for each measurement in the model (both fluxes and fragments)
+
+# For fluxes the the dict is straight forward to understand. It is worth moticing that the input data
+# is stored in the 'data' key and the fitted data is stored in the 'fit' key. I'm not sure if the 'data'
+# is the values corrected natural abundance or the raw values.
+
+# The fragments are a bit more complicated. The dictionary immediately contain overall value e.g. sres and 
+# cont, which the contribution of the full fragement to each of the fitted parameters (reactions). The 'res'
+# key contains a list of dictionaries, one for each fragment. The 'val' is the weighted residual ((fit - data)/std)

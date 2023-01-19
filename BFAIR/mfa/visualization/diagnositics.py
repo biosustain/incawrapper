3a8e8304dc19c7442abf4bfb4498e532a0049f68
viktorht <viktorht@yahoo.dk>
2023-01-19 16:16:58 +0100

    Plot a normal probability plot of the residuals. Has interactive mode which returns an interactive altair chart

--- BFAIR/mfa/visualization/diagnositics.py
+++ BFAIR/mfa/visualization/diagnositics.py
@@ -1,15 +1,76 @@
 import altair as alt
 from BFAIR.mfa.INCA.INCA_results import INCA_results
+import numpy as np
+import pandas as pd
+from typing import Union, Tuple
+import scipy.stats
+import matplotlib.pyplot as plt
 
-def plot_residuals_vs_fitted(INCA_results: INCA_results):
-    df = INCA_results.measurements_and_fit_detailed.drop(columns=['base'])
-    return alt.Chart(df).mark_point().encode(
-        x = alt.X(
-            'fit:Q',
-            scale=alt.Scale(type="log")
-        ),
-        y = alt.Y(
-            'weighted residual:Q',
-        ),
-        tooltip = ['type', 'id:N', 'peak']
+def plot_residuals_vs_fitted(INCA_results: INCA_results) -> alt.Chart:
+    df = INCA_results.measurements_and_fit_detailed.drop(columns=["base"])
+    return (
+        alt.Chart(df)
+        .mark_point()
+        .encode(
+            x=alt.X("fit:Q", scale=alt.Scale(type="log")),
+            y=alt.Y(
+                "weighted residual:Q",
+            ),
+            tooltip=["type", "id:N", "peak"],
+        )
     )
+
+
+def plot_norm_probplot(
+    INCA_results: INCA_results, interactive: bool = False
+) -> Union[alt.Chart, Tuple[plt.Figure, plt.Axes]]:
+
+    if not interactive:
+        fig, ax = plt.subplots()
+        _, _ = scipy.stats.probplot(
+            INCA_results.measurements_and_fit_detailed["weighted residual"],
+            dist="norm",
+            plot=ax,
+        )
+        return fig, ax 
+
+    # Making interactive plot with altair
+    rankits, fit = scipy.stats.probplot(
+        INCA_results.measurements_and_fit_detailed["weighted residual"],
+        dist="norm",
+        plot=None,
+    )
+
+    plot_df = (
+        INCA_results.measurements_and_fit_detailed.sort_values("weighted residual")
+        .drop(columns="base")
+        .copy()
+    )
+    plot_df["Theoretical normal quantile"] = rankits[0]
+
+    points = (
+        alt.Chart(plot_df)
+        .mark_point()
+        .encode(
+            x=alt.X(
+                "Theoretical normal quantile:Q",
+            ),
+            y=alt.Y(
+                "weighted residual:Q",
+            ),
+            tooltip=["type", "id:N", "peak", "weighted residual:Q"],
+        )
+    )
+
+    # Create data for the line
+    x = np.linspace(
+        plot_df["Theoretical normal quantile"].min(),
+        plot_df["Theoretical normal quantile"].max(),
+        10,
+    )
+    y = fit[1] + fit[0] * x
+
+    fit_df = pd.DataFrame({"x": x, "y": y})
+
+    line = alt.Chart(fit_df).mark_line().encode(x="x", y="y")
+    return points + line
---
+++ tests/test_diagnostics.py
@@ -0,0 +1,32 @@
+import pytest
+import altair as alt
+import matplotlib.pyplot as plt
+import pathlib
+from typing import Tuple
+
+current_dir = str(pathlib.Path(__file__).parent.absolute())
+
+from BFAIR.mfa.visualization.diagnositics import plot_residuals_vs_fitted, plot_norm_probplot
+
+
+def test_plot_residuals_vs_fitted(inca_results):
+    """
+    Tests if the plot_residuals_vs_fitted function runs without errors
+    and returns an altair chart
+    """
+    assert type(plot_residuals_vs_fitted(inca_results)) == alt.Chart
+
+def test_plot_norm_probplot_standard_plot(inca_results):
+    """
+    Tests if the plot_norm_probplot function runs without errors and 
+    returns a matplotlib figure
+    """
+    assert type(plot_norm_probplot(inca_results)[0]) == plt.Figure
+
+def test_plot_norm_probplot_interactive_plot(inca_results):
+    """
+    Tests if the plot_norm_probplot with interactive option function
+    runs without errors and returns an altair chart
+    """
+
+    assert type(plot_norm_probplot(inca_results, interactive=True)) == alt.LayerChart

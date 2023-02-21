68f932d8caa165289cdfbc70e7c5d6385151c9bf
viktorht <viktorht@yahoo.dk>
2023-02-21 10:52:04 +0100

    MFA input data documentation now utilise a function to present an overview of the data requirements

--- BFAIR/mfa/INCA/dataschemas.py
+++ BFAIR/mfa/INCA/dataschemas.py
@@ -15,6 +15,9 @@ ExperimentIdColumn = pa.Column(
     description="ID of the experiment. Must be a valid MATLAB variable name, legal characters are a-z, A-Z, 0-9, and the underscore character.",
     checks=pa.Check.str_matches(r'[\w-]+$', error="The experiment_id must be a valid MATLAB variable name, legal characters are a-z, A-Z, 0-9, and the underscore character."),
 )
+ReactionIDColumn = pa.Column(
+    pa.String, required=True, description="The unique id of the reaction"
+)
 
 
 # Define the schema for the model reactions
@@ -22,8 +25,8 @@ ReactionsSchema = pa.DataFrameSchema(
     # TODO: Add validation for reaction arrow
     # TODO: Add validation for id uniqueness
     columns={
-        "rxn_eqn": pa.Column(pa.String, required=True),
-        "rxn_id": pa.Column(pa.String, required=True),
+        "rxn_id": ReactionIDColumn,
+        "rxn_eqn": pa.Column(pa.String, required=True, description="The reaction equation with atom map. Allowed reaction arrows: ->, <->."),
     }
 )
 
@@ -32,24 +35,31 @@ TracerSchema = pa.DataFrameSchema(
     # TODO: Add validation for id uniqueness
     columns={
         "experiment_id": ExperimentIdColumn,
-        "tracer_id": pa.Column(pa.String, required=True, description="Name of the tracer"),
-        "met_id": pa.Column(pa.String, required=True, description="ID of the metabolite"),
+        "tracer_id": pa.Column(pa.String, required=True, description="The unique id of the tracer compound."),
+        "met_id": pa.Column(pa.String, required=True, description="The metabolite id of the labelled compound."),
         "atom_ids": pa.Column(
             pa.String, required=True, 
-            description="""List of labelled atoms in the metabolite. E.g. [1,2] or [C1,C2]. 
-Currently only supports one labelling group, e.i. all labelled atoms have the same purity.""",
-        ),  #  "List of labelled atoms in the metabolite. E.g. [1,2] or [C1,C2]"
-        "atom_mdv": pa.Column(pa.Object, required=True, checks=ContainListsCheck),  # pandera does not support list type
-        "enrichment": pa.Column(pa.Float, required=True, coerce=True),
+            description="""Ids of the labelled atoms in the labelled atom group (equivalent to columns of the same name in the INCA GUI)""",
+        ), 
+        "atom_mdv": pa.Column(pa.Object, required=True, checks=ContainListsCheck, description="""mass/isotopomer distribution vector of the 
+labelled atom group (equivalent to columns of the same name in the INCA GUI). The simplest way to use this column is to specify the purity of 
+the labelling group. This is done supplying a list two numbers, e.g. [0.01, 0.99]."""
+        ),  # pandera does not support list type
+        "enrichment": pa.Column(pa.Float, required=True, coerce=True, description="""mass/isotopomer distribution vector of the labelled atom 
+group (equivalent to columns of the same name in the INCA GUI). The simplest way to use this column is to specify the purity of the labelling 
+group. This is done supplying a list two numbers, e.g. `[0.5, 0.95]` specifies 95% of the compound will be fully labelled in this labelling 
+group. If different atom positions has different purity create a different labelling group for each position. For further description please 
+refer to the INCA manual. Currently, the INCA parser only supports `atom_mdv` of length 2 for each labelling group."""
+        ),
     }
 )
 
 FluxMeasurementsSchema = pa.DataFrameSchema(
     columns={
         "experiment_id": ExperimentIdColumn,
-        "rxn_id": pa.Column(pa.String, required=True),
-        "flux": pa.Column(pa.Float, required=True, coerce=True),
-        "flux_std_error": pa.Column(pa.Float, required=True, coerce=True),
+        "rxn_id": ReactionIDColumn,
+        "flux": pa.Column(pa.Float, required=True, coerce=True, description="Measured/estimated rate typically in mmol/gDW/h"),
+        "flux_std_error": pa.Column(pa.Float, required=True, coerce=True, description="Standard error of the measured/estimated rate"),
     }
 )
 
@@ -60,20 +70,26 @@ MSMeasurementsSchema = pa.DataFrameSchema(
     # labelled_atoms should only one value.
     columns={
         "experiment_id": ExperimentIdColumn,
-        "met_id": pa.Column(pa.String, required=True),
-        "ms_id": pa.Column(pa.String, required=True),
+        "met_id": pa.Column(pa.String, required=True, description="Metabolite ID of metabolite which is directly measured or from which the fragment is derived through a derivatization method."),
+        "ms_id": pa.Column(pa.String, required=True, description="ID of the measured ms fragment - often multiple fragment can be measured from the same metabolite"),
         "measurement_replicate": pa.Column(pa.Int, required=True, coerce=True, 
-            description="Replicate number of the measurement of the same fragment in the same experiment. "
-            "In most cases, the data will only have one measurement per fragment per experiment."
+            description="""Replicate number of the measurement of the same fragment in the same experiment. 
+"In most cases, the data will only have one measurement per fragment per experiment."""
         ),
-        "labelled_atom_ids": pa.Column(pa.Object, required=True, checks=ContainListsCheck),
+        "labelled_atom_ids": pa.Column(pa.Object, required=True, checks=ContainListsCheck, description="""List of atom ids of the labelled atoms in the metabolite."""),
         "unlabelled_atoms": pa.Column(
-            pa.String, required=False, nullable=True, coerce=True,
+            pa.String, required=False, nullable=True, coerce=True, description="""The molecular formula of the all atoms that cannot be labelled through 
+the introduced labels in the tracers. This typically includes non-carbon elements of the fragment and all elements originating from derivatization agent. 
+INCA uses the unlabelled atoms to correct for natural abundance."""
         ),  # nullable=True allows null values as nan or None
-        "mass_isotope": pa.Column(pa.Int, required=True, coerce=True),
-        "intensity": pa.Column(pa.Float, required=True, coerce=True, nullable=True),
-        "intensity_std_error": pa.Column(pa.Float, required=True, coerce=True, nullable=True),
-        "time": pa.Column(pa.Float, required=True, coerce=True),
+        "mass_isotope": pa.Column(pa.Int, required=True, coerce=True, description="""The mass isotopomer of the fragment.
+E.g. M0, M+1, etc. Specified as an integer. It is allowed to have gaps in the isotopmer of a given fragment, e.g. 0, 2, 3. In this case the intensity and 
+std error of missing isotopomers are filled with NaN before inserted in INCA."""
+        ),
+        "intensity": pa.Column(pa.Float, required=True, coerce=True, nullable=True, description="""The measured intensity of the fragment mass isotope."""
+        ),
+        "intensity_std_error": pa.Column(pa.Float, required=True, coerce=True, nullable=True, description="""The standard error of the measured intensity of the fragment mass isotope."""),
+        "time": pa.Column(pa.Float, required=True, coerce=True, description="Time point of measurement only relevant for isotopically non-stationary MFA analysis"),
     },
     checks=[
         # TODO: refactor to accomudate new long input data pa.Check(lambda row: len(row["idv"]) == len(row["labelled_atom_ids"])+1, element_wise=True, raise_warning=True, error="There is not a measurement for all mass isotopes (len(idv)<len(labelled_atom_ids)+1). This is allowed and may as well be intended, but it could be a mistake in input data."),
---
+++ BFAIR/mfa/visualization/schema_overview.py
@@ -0,0 +1,42 @@
+import typing
+import pandas as pd
+import pandera as pa
+from typing import List
+from IPython.display import display, HTML
+
+
+def fetch_column_attributes(
+    schema: pa.DataFrameSchema, column_name: str, attributes: typing.List
+) -> typing.List[str]:
+    """Fetches the values of attributes from a column from a pandera DataFrameSchema"""
+    out = []
+    for attribute in attributes:
+        out.append(getattr(schema.columns[column_name], attribute))
+    return out
+
+
+def schema_overview_df(schema: pa.DataFrameSchema, attributes: typing.List):
+    """Fetch the column attributes from a pandera DataFrameSchema and creates a pandas
+    DataFrame with the column name in the first column and an additional column for each attribute."""
+    schema_overview_dict = {}
+
+    for column in schema.columns:
+        schema_overview_dict[column] = fetch_column_attributes(
+            schema, column, attributes
+        )
+    df = (
+        pd.DataFrame.from_dict(schema_overview_dict, orient="index", columns=attributes)
+        .reset_index()
+        .rename(columns={"index": "column name"})
+    )
+    return df
+
+
+def present_schema_overview(
+    schema: pa.DataFrameSchema,
+    attributes: List = ["dtype", "required", "nullable", "description"],
+) -> None:
+    """Display schema as html for jupyter notebooks. Used for easy documentation."""
+
+    df = schema_overview_df(schema, attributes)
+    display(HTML(df.to_html()))

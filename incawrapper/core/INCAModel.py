8ef693dff8a51c214ba9528b12658be3f1b2fa9e
viktorht <viktorht@yahoo.dk>
2023-10-20 12:44:30 +0200

    model now has property rates The former implementation relied on wrong assumptions of the matlab file format.

--- incawrapper/core/INCAModel.py
+++ incawrapper/core/INCAModel.py
@@ -84,38 +84,91 @@ class INCAModel:
         """
         return pd.DataFrame.from_records(self.raw["states"])
     
+    
     @property
-    def reactions(self) -> pd.DataFrame:
-        """
-        Get a data frame with the reactions and the set flux value. For estimation proceedures the set flux
-        value is the initial guess, and for simulation it is the value used for the simulation. 
-
-        Returns
-        -------
-        pd.DataFrame
-            Dataframe with the reactions
-        """
-        ids = self._extract_reaction_ids()
-        fluxes = self._extra_set_flux_values()
-        return pd.DataFrame(
-            {'rxn_id': ids, 'flux': fluxes}
-        )
-
-    def _extract_reaction_ids(self)->List[str]:
+    def rates(self)->pd.DataFrame:
         """Extract the reaction ids from the raw model data. Reactions that are reversible has two ids
         one for the forward and one for exchange direction.""" 
-        reaction_ids = []
+        rates_collector = []
         for rate in self.raw['rates']:
             if type(rate['flx']) == dict:
-                reaction_ids.append(rate['id'])
+                rates_collector.append(_clean_flx_dict(rate['flx']))
             if type(rate['flx']) == np.ndarray:
                 for direction in rate['flx']:
-                    reaction_ids.append(direction['id'])
-        return reaction_ids
+                    rates_collector.append(_clean_flx_dict(direction))
+        return pd.DataFrame.from_records(rates_collector)
+    
+    @property
+    def rates_in_net_exch_format(self) -> pd.DataFrame:
+        return _convert_reactions_to_net_exch_format(self.rates)
 
-    def _extra_set_flux_values(self):
-        """Extra the set fluxes from a inca model rates object. A vector of all the set fluxes
-        is stored in each rate object. Here we simply extract the information from the first. 
-        The order of the fluxes is the same as the order of the reaction ids."""
-        return self.raw['rates'][0]['flx']['val']
 
+def _convert_reactions_to_net_exch_format(df)->pd.DataFrame:
+    """In INCA the reversible reactions are stored as a forward and backward reaction. However,
+    sometimes INCA represents the reactions as a net and exchange reaction. This function converts
+    the reversible reactions to net and exchange reactions. The method for convertion is found in
+    the INCA Manual.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe with the reactions. The dataframe must have the columns `rxn_id` and `flux`.
+        Usually obtain from the `reactions` property of the `INCAModel` class.
+    
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with the reactions in net and exchange format.
+    
+    Notes
+    -----
+    This function is separeted from the `INCAModel` class, to ease testing.
+    """
+    df=df.copy()
+
+    new_reactions_df = pd.DataFrame()
+    for rxn_id, dat in df.groupby('rxn'):
+        if dat.shape[0] == 2:
+            forward_flux = dat[dat['dir'] == 'f']['val'].values[0]
+            backward_flux = dat[dat['dir'] == 'b']['val'].values[0]
+
+            net_flux = forward_flux - backward_flux
+            exch_flux = min([forward_flux, backward_flux])
+            rxn_df = pd.DataFrame.from_dict(
+                {
+                    'rxn_id': [f'{rxn_id} net', f'{rxn_id} exch'],
+                    'flux': [net_flux, exch_flux],
+                }
+            ) 
+
+            new_reactions_df = pd.concat([
+                new_reactions_df,
+                rxn_df 
+            ])
+        else:
+            new_reactions_df = pd.concat([
+                new_reactions_df,
+                dat[['rxn', 'val']].rename(columns={'rxn': 'rxn_id', 'val': 'flux'})
+            ])
+
+    return new_reactions_df.reset_index(drop=True)
+
+def _clean_flx_dict(flx_dict):
+    """Cleans the flux dictionary. Removes the keys that are not needed.
+
+    Parameters
+    ----------
+    flx_dict : Dict
+        Dictionary with the flux information.
+    
+    Returns
+    -------
+    Dict
+        Dictionary with the parsed flux information.
+    """
+    # remove the keys that are not needed
+    flx_dict.pop('prod', None)
+    flx_dict.pop('sub', None)
+    flx_dict.pop('base', None)
+
+    return flx_dict
